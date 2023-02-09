1335b7a52328fec12655c65832704f053864449b
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-09 18:03:45 -0300

    regex

--- src/string/index.js
+++ src/string/index.js
@@ -2,4 +2,8 @@ const url = "sm.mypharma.com.br"
 
 const newurl = url.split('.')[1]
 
+const street = "rua, da, lapa"
+const fixed_street = street.replace(/\,/g, '')
+
+console.log(fixed_street)
 console.log(newurl)
